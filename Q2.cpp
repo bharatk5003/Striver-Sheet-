196b235c87b00229862537a23a285e257d90bcca
Bharat <bharatk503@gmail.com>
2021-12-23 00:52:10 +0530

    second commit

---
+++ Q2.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	// for getting input from input.txt
+	freopen("input.txt", "r", stdin);
+	// for writing output to output.txt
+	freopen("output.txt", "w", stdout);
+#endif
+
+
+	int n;
+	cout << "Hello";
+	cin >> n;
+	cout << 5 * n << endl;
+
+
+
+
+}
